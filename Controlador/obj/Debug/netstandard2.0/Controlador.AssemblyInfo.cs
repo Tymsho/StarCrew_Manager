99c88aec3fc70a87aa1107b2399f21ce7b81a943
Tymsho <martinnzlm@gmail.com>
2025-05-12 23:42:54 -0300

    Version 1.0

--- Controlador/obj/Debug/netstandard2.0/Controlador.AssemblyInfo.cs
+++ Controlador/obj/Debug/netstandard2.0/Controlador.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Controlador")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad4305312c66e81f59b93c9b6241e8bfdb739db7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Controlador")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Controlador")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
